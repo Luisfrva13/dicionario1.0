04e47326157e89efdee77415d6c53f01533d216e
Luis_Fernando <luisfernandoveras13@gmail.com>
2024-05-28 21:07:12 -0300

    versão 1.1

--- main.py
+++ main.py
@@ -16,18 +16,22 @@
 
 import os
 
-pessoas = {'Nome':input('Informe o nome do usuário:'),
-         'CPF':input('Informe o CPF do usuário:'),
-         'RG':input('Informe o RG do usuário:'),
-         'Data de Nascimento':input('Informe a data de nascimento do usuário:'),
-         'Gênero':input('Informe o gênero do usuário:'),
-         'E-mail':input('Informe o E-mail do usuário:'),
-         'Telefone':input('Informe o telefone do usuário:'),
-         'Tipo Sanguíneo':input('Informe o tipo sanguíneo do usuário:'),
-         'Profissão':input('Informe a profissão do usuário:'),
-         'Empresa':input('Informe a empresa do usuário:')}
+pessoa = {
+        'Nome':input('Informe o nome do usuário:'),
+        'CPF':input('Informe o CPF do usuário:'),
+        'RG':input('Informe o RG do usuário:'),
+        'Data de Nascimento':input('Informe a data de nascimento do usuário:'),
+        'Gênero':input('Informe o gênero do usuário:'),
+        'E-mail':input('Informe o E-mail do usuário:'),
+        'Telefone':input('Informe o telefone do usuário:'),
+        'Tipo Sanguíneo':input('Informe o tipo sanguíneo do usuário:'),
+        'Profissão':input('Informe a profissão do usuário:'),
+        'Empresa':input('Informe a empresa do usuário:')
+         }
 
 os.system('cls')
 
-for chave in pessoas:
-    print(f'{chave}: {pessoas.get(chave)}')
+for chave in pessoa:
+    print(f'{chave}: {pessoa.get(chave)}')
+
+print('')
